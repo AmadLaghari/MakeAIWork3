613c571cdf32ccd4ed06d5dee0f52d3a780b5f8f
AmadLaghari <0825637@hr.nl>
2022-12-01 09:34:52 +0100

    python shapes

---
+++ opdrachten/practica/week13/opdrachten/shape/triangle.py
@@ -0,0 +1,12 @@
+from shape import Shape, math
+from shape import math
+
+class Triangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return round(0.5 * self.width * self.height * math.pi)
+
+Triangle(2)
